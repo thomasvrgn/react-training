b8843896ccf556a383034983459a02f761918d58
thomasvergne <thomas@quark-lang.dev>
2021-04-30 11:43:35 +0200

    🚸 Default value for empty todo added.

--- src/components/List.js
+++ src/components/List.js
@@ -6,26 +6,35 @@ export const List = () => {
 
   return (
     <div className="flex justify-center my-8">
-      <ul className="w-2/3 space-y-4">
-        {todo.map((item, index) => {
-          return (
-            <li key={index} className="flex flex-row">
-              <span className="flex flex-auto bg-gray-100 py-1.5 rounded-l-lg items-center px-4 font-medium text-gray-700">
-                <input 
-                  type="text" 
-                  defaultValue={item.value}
-                  onChangeCapture={(e) => changeTodo(e.target.value, index)}
-                  className="bg-gray-100 focus:outline-none" />
-              </span>
-              <button
-                className="bg-red-400 py-1.5 px-4 rounded-r-lg text-sm text-white font-medium tracking-wide"
-                onClick={() => removeTodo(index)}>
-                Supprimer
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      {
+        todo.length > 0 ? 
+        (
+          <ul className="w-2/3 space-y-4">
+            {todo.map((item, index) => {
+              return (
+                <li key={index} className="flex flex-row">
+                  <span className="flex flex-auto bg-gray-100 py-1.5 rounded-l-lg items-center px-4 font-medium text-gray-700">
+                    <input 
+                      type="text" 
+                      defaultValue={item.value}
+                      onChangeCapture={(e) => changeTodo(e.target.value, index)}
+                      className="bg-gray-100 focus:outline-none" />
+                  </span>
+                  <button
+                    className="bg-red-400 py-1.5 px-4 rounded-r-lg text-sm text-white font-medium tracking-wide"
+                    onClick={() => removeTodo(index)}>
+                    Supprimer
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <h1 className="text-lg font-medium text-gray-700">
+            Aucune tâche n'a encore été ajoutée
+          </h1>
+        )
+      }
     </div>
   );
 }
